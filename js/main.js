365e4cf9070b3b69159a958cd7755d811fbf4541
Zachary Lincoln <zach@burlingtoncollective.com>
2014-10-04 16:17:48 -0400

    navigation show/hide logic when hero present

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
 $(function(){
-	$('#home-hero').css({
-		height: $(window).height() + 'px'
-	});
-	$(window).on('resize', function(){
-		$('#home-hero').css({
+	
+	var $hero = $('#home-hero');
+
+	if($hero.length > 0){
+		
+		var $window = $(window),
+				$body = $('body');
+
+		$body.addClass('on-home');
+
+		$hero.css({
 			height: $(window).height() + 'px'
 		});
-	});
+		
+		$window.on('resize', function(){
+			$hero.css({
+				height: $(window).height() + 'px'
+			});
+		});
+		$window.scroll(function(){
+			if(!$body.is('transitional')){
+				$body.addClass('transitional');
+			}
+			if($window.scrollTop() > ($hero.height() / 2)){
+				$body.addClass('show-nav');
+			}else{
+				$body.removeClass('show-nav');
+			}
+		});
+
+	}
+
 });
