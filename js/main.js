8bc023eafcd57fbd2a7f617da2d5d7037529dcfe
Zachary Lincoln <zach@burlingtoncollective.com>
2014-10-28 04:32:56 -0400

    a lot of design and photos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 $(function(){
+
+	function getUrlParameter(sParam)
+	{
+		var sPageURL = window.location.search.substring(1);
+		var sURLVariables = sPageURL.split('&');
+		for (var i = 0; i < sURLVariables.length; i++) 
+		{
+			var sParameterName = sURLVariables[i].split('=');
+			if (sParameterName[0] == sParam) 
+			{
+				return sParameterName[1];
+			}
+		}
+	}
 	
 	var $hero = $('#home-hero');
 
@@ -29,11 +43,44 @@ $(function(){
 			}
 		});
 
-		if($hero.is('.example')){
-			$hero.backstretch([
-				"images/will2.jpg",
-				"images/carly2.jpg"
-			], {fade: 500});
+		switch (getUrlParameter('theme')){
+			case 'forest':
+				$hero.backstretch([
+					"images/downed-tree.png",
+					"images/stump.png",
+					"images/moss.png",
+					"images/sky.png"
+				], {fade: 500});
+				break;
+			case 'other':
+				$hero.backstretch([
+					"images/bridge.png",
+					'images/stock/harbor-wide.jpg', //other
+					'images/stock/road-truck.jpg', //looks good, not sure what saying
+					'images/stock/brick-city.jpg', //loud
+					'images/stock/gold-bridge.jpg',  //odd placement
+					'images/stock/historic-ship.jpg'  //odd placement
+				], {fade: 500});
+				break;
+			case 'color':
+				$hero.backstretch([
+					"images/bridge.png",
+					'images/stock/crabs.jpg',  //loud
+					'images/stock/dusk-city.jpg', //color
+					'images/stock/city-neon.jpg', //loud
+					'images/stock/sunset-grass.jpg', //color
+				], {fade: 500});
+				break;
+			default:
+				$hero.backstretch([
+					'images/stock/under-dock.jpg', //great
+					'images/stock/rocky-shore.jpg', //good
+					'images/stock/arial-island.jpg', //great
+					'images/stock/bridge-go.jpg', //looks good, not sure what saying
+					'images/stock/gold-bridge-framed.jpg',  //great
+					'images/stock/gold-bridge-side.jpg', //great
+				], {fade: 500});
+				break;
 		}
 
 	}
